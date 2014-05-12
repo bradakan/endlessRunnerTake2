ae4b5fa5118a2c3017fab137fea84c810684448a
Henk Raamkozijn <koen5067@icloud.com>
2014-05-12 16:33:03 +0200

    dit is het eerste stukje

---
+++ Endless_Runner/Assets/_Scripts/CameraBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviourScript : MonoBehaviour {
+
+	public int movement = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (new Vector2 (movement * Time.deltaTime, 0));
+	
+	}
+}
---
+++ Endless_Runner/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	bool playerJump;
+	float gravitiScale = 5;
+	float jumpForce = 1000;
+	// Use this for initialization
+	void Start () 
+	{
+		playerJump = false;
+		rigidbody2D.gravityScale = gravitiScale;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(transform.position.x <= 0)
+		{
+			transform.Translate(1 * Time.deltaTime,0,0);
+		}
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			if(playerJump == false && rigidbody2D.gravityScale == gravitiScale)
+			{
+				rigidbody2D.AddForce(new Vector2(0,jumpForce));
+				playerJump = true;
+			}
+			if(playerJump == false && rigidbody2D.gravityScale == -gravitiScale)
+			{
+				rigidbody2D.AddForce(new Vector2(0,-jumpForce));
+				playerJump = true;
+			}
+		}
+		if(Input.GetKeyDown (KeyCode.W))
+		{
+			if(rigidbody2D.gravityScale == gravitiScale)
+			{
+				rigidbody2D.gravityScale = -gravitiScale;
+			}
+			else{rigidbody2D.gravityScale = gravitiScale;}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		playerJump = false;
+	}
+}
